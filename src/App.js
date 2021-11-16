b07902efe24bfc79001baf48bfc60b2dae03ee94
leo <leo.mollard@usherbrooke.ca>
2021-11-16 09:17:32 -0500

    redux persist store

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import Navbar from "./components/Navbar";
 import store from './store'
 import {Provider} from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
 function App() {
-
-  return (
+    let persistor = persistStore(store);
+    return (
       <Provider store={store}>
-          <div className="App">
-              <Navbar/>
-          </div>
+          <PersistGate loading={null} persistor={persistor}>
+              <div className="App">
+                  <Navbar/>
+              </div>
+          </PersistGate>
       </Provider>
-  );
+    );
 }
 
 export default App;
--- src/store.js
+++ src/store.js
@@ -1,10 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import matchsReducer from './reduxSlice/matchsSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer } from 'redux-persist'
 import idReducer from './reduxSlice/idSlice'
+import thunk from 'redux-thunk'
 
-export default configureStore({
-    reducer: {
+const reducers = combineReducers(
+    {
         matchs: matchsReducer,
         id: idReducer
-    },
+    }
+);
+
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default configureStore({
+    reducer: persistedReducer,
+    middleware: [thunk]
 })
